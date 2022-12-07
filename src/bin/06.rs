8a5b432367ba5349990258e8d78462fbfbb79579
Burkhard Mittelbach <wasabi37a@googlemail.com>
2022-12-07 18:02:40 +0100

    Day 6

---
+++ src/bin/06.rs
@@ -0,0 +1,56 @@
+fn has_dup<T: PartialEq>(slice: &[T]) -> bool {
+    for i in 1..slice.len() {
+        if slice[i..].contains(&slice[i - 1]) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    find_start_index(input, 4)
+}
+
+fn find_start_index(input: &str, marker_size: u32) -> Option<u32> {
+    let mut index = marker_size;
+    for window in input.as_bytes().windows(marker_size as usize) {
+        if !has_dup(window) {
+            return Some(index);
+        }
+        index += 1;
+    }
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    find_start_index(input, 14)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(7));
+        assert_eq!(part_one("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
+        assert_eq!(part_one("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
+        assert_eq!(part_one("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(10));
+        assert_eq!(part_one("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(19));
+        assert_eq!(part_two("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(23));
+        assert_eq!(part_two("nppdvjthqldpwncqszvftbrmjlhg"), Some(23));
+        assert_eq!(part_two("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(29));
+        assert_eq!(part_two("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(26));
+    }
+}
